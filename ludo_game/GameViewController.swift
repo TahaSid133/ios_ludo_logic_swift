cc1a0bd81c198dc0d092b7fd874fe528053d74df
Taha <luqmaan.siddiqui13@gmail.com>
2018-08-22 23:23:37 -0400

    Update GameViewController.swift

--- ludo_game/GameViewController.swift
+++ ludo_game/GameViewController.swift
@@ -8,22 +8,7 @@
 
 import UIKit
 
-struct tokenTraker{
-    var position:Int
-    var universalPosition:Int
-}
 
-struct Tokens{
-    var token_one:tokenTraker
-    var token_two:tokenTraker
-    var token_three:tokenTraker
-    var token_four:tokenTraker
-}
-
-struct player{
-    var playerNumber:Int
-    var tokens:Tokens
-}
 
 
 
